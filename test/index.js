e22ec959896e41129745dcc0024af37e90b9488f
Kenneth Lee <kenneth.lee@cbc.ca>
2014-02-18 16:48:37 +0000

    added express test

--- test/index.js
+++ test/index.js
@@ -29,24 +29,34 @@ describe('websocket server', function() {
         });
     });
 
+    it('should integrate with express', function(done) {
+        var app = require('express')();
+            server = new Server(app),
+            gameroom = new GameRoom(server, mockOptions);
+
+        request(server)
+            .get('/socket.io/socket.io.js')
+            .expect(200, done);
+    });
+
     it.skip('should disconnect user and remove user from room', function(done) {
         var roomName = uid(),
             server = new Server(),
             gameroom = new GameRoom(server, mockOptions);
-        
+
         var client1 = connectSocket(server, { multiplex: false }),
             client2 = connectSocket(server, { multiplex: false });
-        
+
         client1.emit('create', roomName, function() {
             client2.emit('join', roomName, function() {
                 gameroom.cmd.smembers(keyJoin('rooms', roomName, 'sockets'), function(err, sockets) {
                     console.log(sockets);
                     sockets.length.should.equal(2);
                     client2.disconnect();
-                });                
+                });
             });
         });
-        
+
         setTimeout(function() {
             gameroom.cmd.smembers(keyJoin('rooms', roomName, 'sockets'), function(err, sockets) {
                 console.log(sockets);
