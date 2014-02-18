f80cd79b935105b955cb424b80b8db3b1fb76422
Kenneth Lee <kennethkl@gmail.com>
2014-02-18 02:36:02 +0000

    completed messaging, even between servers

--- test/common/index.js
+++ test/common/index.js
@@ -7,6 +7,16 @@ exports.connectSocket = function(server, opts) {
     return Client('ws://' + addr.address + ':' + addr.port, opts);
 };
 
+exports.connectSocketAndIdentify = function(server, opts) {
+    var client = exports.connectSocket(server, opts);
+    
+    client.on('identity', function(id) {
+        client.login = id;
+    });
+    
+    return client;
+};
+
 exports.mockOptions = {
     pub: redis,
     sub: redis,
--- test/handlers/message.js
+++ test/handlers/message.js
@@ -1,35 +1,83 @@
-var uid = require('uid');
-    //message = require('../../lib/message');
+var Server = require('http').Server,
+    should = require('should'),
+    uid = require('uid'),
+    connectSocket = require('../common').connectSocket,
+    connectSocketAndIdentify = require('../common').connectSocketAndIdentify,
+    mockOptions = require('../common').mockOptions,
+    GameRoom = require('../..');
 
 describe('Server `message` handler', function() {
 
-    it.skip('should pass message to logged in player');
-
-    it.skip('should error when target player does not exist', function(done) {
-        var playerName = uid(),
-            mockedSocket = {
-                emit: function(key, value) {
-                    key.should.equal('message');
-                    value.should.have.property('error').and.equal('player does not exist');
-
-                    done();
-                }
-            };
-        message.call(mockedSocket, {player: playerName});
+    it('should pass message to logged in player', function(done) {
+         var server = new Server(),
+            gameroom = new GameRoom(server, mockOptions);
+        
+        var client1 = connectSocketAndIdentify(server, { multiplex: false }),
+            client2 = connectSocket(server, { multiplex: false });
+        
+        client2.on('identity', function(login) {
+            client2.login = login;
+            
+            // Get client1 to send to client2
+            client1.emit('message', {player: login, message: 'test'}, function(err, data) {
+                data.should.have.property('player').and.equal(client1.login);
+                data.should.have.property('message').and.equal('test');
+            });
+        });
+        
+        client2.on('message', function(data) {
+            data.should.have.property('player').and.equal(client1.login);
+            data.should.have.property('message').and.equal('test');
+            done();
+        });
     });
 
-    it('should pass message to room');
-    it.skip('should error when room does not exist', function(done) {
-        var mockedSocket = {
-            emit: function(key, value) {
-                key.should.equal('message');
-                value.should.have.property('error').and.equal('room does not exist');
-
-                done();
-            }
-        };
-
-        message.call(mockedSocket, '');
+    it('should pass message to room', function(done) {
+         var server = new Server(),
+            gameroom = new GameRoom(server, mockOptions),
+            roomName = uid();
+        
+        var client1 = connectSocketAndIdentify(server, { multiplex: false }),
+            client2 = connectSocketAndIdentify(server, { multiplex: false });
+        
+        client1.emit('create', roomName, function() {
+            client2.emit('message', {room: roomName, message: 'test'});
+        });
+        
+        client1.on('message', function(data) {
+            data.should.have.property('room').and.equal(roomName);
+            data.should.have.property('player').and.equal(client2.login);
+            data.should.have.property('message').and.equal('test');
+            done();
+        });
     });
+    
+    it('should pass message to player in different server', function(done) {
+        var server1 = new Server(),
+            server2 = new Server(),
+            gameroom1 = new GameRoom(server1, mockOptions),
+            gameroom2 = new GameRoom(server2, mockOptions),
+            roomName = uid();
+        
+        var client1 = connectSocketAndIdentify(server1, { multiplex: false }),
+            client2 = connectSocket(server2, { multiplex: false });
+        
+        client2.on('identity', function(login) {
+            client2.login = login;
+            
+            // Get client1 to send to client2
+            client1.emit('message', {player: login, message: 'test'}, function(err, data) {
+                data.should.have.property('player').and.equal(client1.login);
+                data.should.have.property('message').and.equal('test');
+            });
+        });
+        
+        client2.on('message', function(data) {
+            data.should.have.property('player').and.equal(client1.login);
+            data.should.have.property('message').and.equal('test');
+            done();
+        });
+    });
+
 
 });
