add7fb37b1d362a93333c74a3a911f704cea1f39
Kenneth Lee <kenneth.lee@cbc.ca>
2014-02-24 19:19:29 +0000

    fixed leave bug

--- test/handlers/create.js
+++ test/handlers/create.js
@@ -13,22 +13,22 @@ describe('Server `create` Handler', function() {
             roomName = uid();
 
         var client = connectSocket(server);
-        
+
         client.emit('create', roomName, function() {
             gameroom.sockets.sockets[0].rooms.indexOf(roomName).should.equal(1);
             done();
         });
     });
-    
+
     it('should error when creating an existing game', function(done) {
         var server = new Server(),
             gameroom = new GameRoom(server, mockOptions()),
             roomName = uid();
 
         var client = connectSocket(server);
-        
+
         client.emit('create', roomName, function() {
-            
+
             // Create again!
             client.emit('create', roomName, function(err) {
                 err.should.have.property('error').and.equal('room already created');
